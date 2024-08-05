4aca7c55f2d9035d244ff8e614acf8e58cf7b7c6
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2024-08-05 23:30:41 +0000

    Fix: datetime object nor serializable

--- dredd_test_runners/llvm_regression_tests_runner/main.py
+++ dredd_test_runners/llvm_regression_tests_runner/main.py
@@ -191,7 +191,7 @@ def main():
                     mutant_path.mkdir()
                     print("Writing kill info to file.")
                     with open(mutant_path / "kill_info.json", "w") as outfile:
-                        json.dump({"killing_test": test_filename_without_prefix, "kill_type": str(mutant_result), "kill_timestamp": datetime.datetime.now()},
+                        json.dump({"killing_test": test_filename_without_prefix, "kill_type": str(mutant_result), "kill_timestamp": str(datetime.datetime.now())},
                                   outfile)
                 except FileExistsError:
                     print(f"Mutant {mutant} was independently discovered to be killed.")
@@ -217,8 +217,8 @@ def main():
                            "killed_mutants": killed_by_this_test,
                            "skipped_mutants_count": len(already_killed_by_other_tests),
                            "survived_mutants_count": len(covered_but_not_killed_by_this_test),
-                           "analysis_start_time": analysis_timestamp_start,
-                           "analysis_end_time": analysis_timestamp_end,
+                           "analysis_start_time": str(analysis_timestamp_start),
+                           "analysis_end_time": str(analysis_timestamp_end),
                            }, outfile)
 
 
